ef53eff0ad7e6a58962d014e6f89cb1fc62df6e8
Narei <ieranofficial@gmail.com>
2023-05-03 13:17:08 +0200

    export

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ const nextConfig = {
     EMAIL_SERVICE_ID: process.env.EMAIL_SERVICE_ID,
     EMAIL_TEMPLATE_ID: process.env.EMAIL_TEMPLATE_ID,
     EMAIL_PUBLIC_KEY: process.env.EMAIL_PUBLIC_KEY
-  }
+  },
+  output: 'export'
 }
 
 module.exports = nextConfig
